a5a1fab6bcc02ba84d76ea0fcff6007cc5b34dee
Alexandre Quessy <alexandre@quessy.net>
2013-06-28 11:21:26 -0400

    proto_01.py kind of works

--- poet/README.rst
+++ poet/README.rst
@@ -6,3 +6,8 @@ Veille technologique
 
 * Python NLTK: http://nltk.org/book/
 
+
+Faudrait changer l'ordre des phrases de la source avant de générer du nouveau texte.
+
+* http://stackoverflow.com/questions/1150144/generating-random-sentences-from-custom-text-in-pythons-nltk
+
---
+++ poet/proto_01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+One must do this first::
+
+  import nltk
+  nltk.download()
+
+"""
+#from __future__ import division
+import nltk
+#import re
+#import pprint
+
+FILE_NAME = "pg43038.txt"
+
+def hello(length=100):
+    raw = open(FILE_NAME, "rU").read()
+    tokens = nltk.word_tokenize(raw)
+    text = nltk.Text(tokens)
+    result = text.generate(length)
+
+if __name__ == "__main__":
+    hello()
+
