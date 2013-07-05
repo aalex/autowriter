b6eede3ae2d94a0297c5e55c034319ed4c4aca5e
Alexandre Quessy <alexandre@quessy.net>
2013-07-05 19:54:21 -0400

    had forgotten to add plotter.py

---
+++ autowriter/plotter.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+"""
+Sends/receives to/from a plotter over USB.
+Author: Alexandre Quessy
+Date: July 2013
+"""
+
+from twisted.python import log
+from twisted.python import usage
+from twisted.internet import reactor
+from twisted.internet import defer
+from twisted.internet import task
+from twisted.internet.serialport import SerialPort
+from twisted.protocols import basic
+import sys
+import os
+
+class PlotterProtocol(basic.LineReceiver):
+    """
+    Simple protocol to send and receive ASCII to/from a plotter.
+    """
+    delimiter = ";"
+    bufferSize = 1 # XXX
+
+    def lineReceived(self, line):
+        log.msg("Received: \"%s\"" % (line))
+
+#def slowly_write(serial_port, text):
+#    def _later(new_text):
+#        c = new_text[0:1]
+#        log.msg("writing %s" % (c))
+#        serial_port.write(c)
+#        new_text = new_text[1:]
+#        if len(new_text) == 0:
+#            log.msg("done writing")
+#        else:
+#            reactor.callLater(0.01, _later, new_text)
+#    _later(text)
+
+class Spooler(object):
+    """
+    Writes text one character at a time to a given serial port, waiting between each.
+    Printers and plotters are slow devices.
+    """
+    def __init__(self, serial_port):
+        INTERVAL = 0.01
+        self._serial_port = serial_port
+        self._to_write = ""
+        self._looping_call = task.LoopingCall(self._periodical_write)
+        # Start the task:
+        self._looping_call.start(INTERVAL, True)
+    
+    def _periodical_write(self):
+        if self.has_to_write():
+            c = self._to_write[0:1]
+            log.msg("writing %s" % (c))
+            self._serial_port.write(c)
+            self._to_write = self._to_write[1:]
+            if not self.has_to_write():
+                log.msg("done writing")
+
+    def append(self, text):
+        self._to_write += text
+
+    def has_to_write(self):
+        return len(self._to_write) != 0
+
+class Options(usage.Options):
+    """
+    Command line options for this program
+    """
+    optFlags = [
+        ["verbose", "v", "Print many info"],
+    ]
+    optParameters = [
+        ["outfile", "o", None, "Logfile [default: sys.stdout]"],
+        ["baudrate", "b", None, "Serial baudrate [default: 9600]"],
+        ["file", "f", None, "File name to read the HPGL from."],
+        ["port", "p", '/dev/ttyACM0', "Serial Port device"],
+    ]
+
+def query_output_actual_point(serial_port):
+    """
+    Sends OA;
+    """
+    log.msg("Write OA;")
+    s.write("OA;")
+    return defer.succeed(None)
+
+def send_file_contents(slow_writer, file_name):
+    """
+    Sends the contents of a file.
+    """
+    if os.path.isfile(file_name):
+        if os.access(file_name, os.R_OK):
+            log.msg("Write file contents from %s" % (file_name))
+            with open(file_name, "rU") as f:
+                contents = f.read()
+                slow_writer.append(contents)
+                # s.write(contents)
+                return defer.succeed(None)
+        else:
+            log.msg("File is not readable: %s" % (file_name))
+            return defer.fail(RuntimeError("File is not readable"))
+    else:
+        log.msg("File does not exist: %s" % (file_name))
+        return defer.fail(RuntimeError("File does not exist"))
+
+if __name__ == '__main__':
+    options = Options()
+    # defaults
+    baudrate = 9600
+    filename = None
+    port = None
+    try:
+        options.parseOptions()
+    except usage.UsageError, errortext:
+        print "%s: %s" % (sys.argv[0], errortext)
+        print "%s: Try --help for usage details." % (sys.argv[0])
+        raise SystemExit, 1
+
+    # Logging option
+    logFile = options.opts["outfile"]
+    if logFile is None:
+        logFile = sys.stdout
+    log.startLogging(logFile)
+
+    # Verbose
+    if options.opts['verbose']:
+        print("TODO: be verbose")
+    # Baud rate
+    if options.opts["baudrate"]:
+        baudrate = int(options.opts["baudrate"])
+    if options.opts["file"]:
+        filename = options.opts["file"]
+    # Port
+    port = options.opts["port"]
+
+    # Open it
+    log.msg("Attempting to open %s at %dbps as a %s device" % (port, baudrate, PlotterProtocol.__name__))
+    s = SerialPort(PlotterProtocol(), port, reactor, baudrate=baudrate)
+    writer = Spooler(s)
+
+    if filename is None:
+        log.msg("No file name provided.")
+    else:
+        if os.path.isfile(filename) and os.access(filename, os.R_OK):
+            reactor.callLater(0.0, send_file_contents, writer, filename)
+        else:
+            log.msg("File does not exist or is not readable: %s" % (filename))
+
+    #reactor.callLater(0.0, query_output_actual_point, s)
+    reactor.run()
+
